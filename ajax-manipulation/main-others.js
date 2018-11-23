a8e347e3d45b5bf48dfb295f009865439633eaa1
abhi15494 <abhishek15494@gmail.com>
2018-11-23 20:25:15 +0530

    new promise api

--- ajax-manipulation/main-others.js
+++ ajax-manipulation/main-others.js
@@ -41,8 +41,6 @@ $(document).ready(function(){
             }
         });
     }
-    console.log("data");
-
     function handlecallbackdata(data){
         return data.map((res)=>
         `<li class="list-group-item"=>${res.id} ).  ${res.title}</li>`
@@ -131,6 +129,7 @@ window.onload = function(){
                     return handle(gen.next(data));
                 });
             }
+            console.log(yielded);
         }
         return handle(gen.next());
     }
@@ -145,3 +144,37 @@ window.onload = function(){
     $btn3 = $(".btn3");
     $btn3.on("click", gen);
 }
+
+// postData(`http://example.com/answer`, {answer: 42})
+//   .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+//   .catch(error => console.error(error));
+
+// function postData(url = ``, data = {}) {
+//   // Default options are marked with *
+//     return fetch(url, {
+//         method: "POST", // *GET, POST, PUT, DELETE, etc.
+//         mode: "cors", // no-cors, cors, *same-origin
+//         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//         credentials: "same-origin", // include, *same-origin, omit
+//         headers: {
+//             "Content-Type": "application/json; charset=utf-8",
+//             // "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         redirect: "follow", // manual, *follow, error
+//         referrer: "no-referrer", // no-referrer, *client
+//         body: JSON.stringify(data), // body data type must match "Content-Type" header
+//     })
+//     .then(response => response.json()); // parses response to JSON
+// }
+
+// Fetch data
+
+const apifetch = fetch('https://jsonplaceholder.typicode.com/todos');
+( async()=> {
+    let datag = await apifetch;
+    let data = await datag.json();
+    console.log(data);
+    const newdata = data.filter( data => data.id < 30? true: false )
+    // data.map(d => `${d.id}). title: ${d.title} \n`)
+    console.log(...newdata);
+})();
